6c0931e0a3350a06a0da7c43ab34669941f81de4
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2025-06-17 09:18:59 +0200

    Fix response for existing or non-existing mail while sending reset password mail, add dummmy route for password.reset

--- lang/pl/passwords.php
+++ lang/pl/passwords.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 return [
     "reset" => "Hasło zostało zresetowane!",
-    "sent" => "Link do zresetowania hasła został wysłany na e-mail.",
+    "sent" => "Jeżeli Twój adres e-mail jest w naszej bazie, to otrzymasz wiadomość z linkiem do zresetowania hasła",
     "throttled" => "Proszę zaczekać zanim spróbujesz ponownie.",
     "token" => "Token resetowania hasła jest nieprawidłowy.",
-    "user" => "Token resetowania hasła jest nieprawidłowy.",
 ];
