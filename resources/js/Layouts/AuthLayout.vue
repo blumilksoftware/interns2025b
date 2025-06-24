7b23f670272943adbb5c266db7a0b83ecff6c5ac
Jakub Demkowski <jakub.demkowski@studenci.collegiumwitelona.pl>
2025-06-24 13:16:34 +0200

    **Refactor authentication components and layouts; update routes**

--- resources/js/Layouts/AuthLayout.vue
+++ resources/js/Layouts/AuthLayout.vue
@@ -5,18 +5,10 @@ import AppHead from '@/Components/AppHead.vue'
 
 <template>
   <AppHead title="Dashboard" description="This is the dashboard page" />
-  <div class="flex-auto grid grid-cols-2 gap-4 auto-rows-fr justify-center bg-gradient-to-tr from-[#44A08D] to-[#093637] h-screen">
-    <div class="flex justify-center items-center text-center p-4">
-      <slot name="left" />
-    </div>
-    <div class="flex justify-center items-center text-center p-4">
-      <slot name="right" />
+  <div class="flex flex-col items-center justify-center bg-gray-50 h-screen ">
+    <div class="w-full max-w-md">
+      <slot name="auth" />
     </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
-
 
--- resources/js/Pages/Auth/LoginPage.vue
+++ resources/js/Pages/Auth/LoginPage.vue
@@ -1,40 +1,31 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import AuthLayout from '@/Layouts/AuthLayout.vue'
-import BaseInput from '@/Components/BaseInput.vue'
 
-
-const email = ref('')
-const password = ref('')
 </script>
 
 <template>
   <AuthLayout>
-    <template #left>
-      <div class="flex-auto bg-gray-200 min-w-[300px] max-w-[70%]">
-        <h2>Welcome back</h2>
-        <form @submit.prevent="submit">
-          <BaseInput
-            v-model="email"
-            placeholder="Email"
-            error="Niepoprawny format"
-          />
-          <BaseInput
-            v-model="password"
-            type="password"
-            placeholder="Hasło"
-          />
-          <button class="" type="submit">Zaloguj się</button>
+    <template #auth>
+      <div class=" flex-auto bg-gray-50 min-w-[300px] max-w-screen-md rounded-lg">
+        <div class="[&>*]:ml-9 pb-20 text-xl text-white bg-gradient-to-tr from-[#025F60] to-[#093637] rounded-lg">
+          <h2 class=" font-bold">Zaloguj się</h2>
+          <p class=" font-semibold">Nie posidasz konta? <a href="" class="text-white font-bold"><span class="underline font-bold align">Zarejestruj się</span></a></p>
+        </div>
+        <form class="[&>*]:ml-9 text-xl mt-4" action="#" method="post">
+          <p class="text-base text-gray-500">Email</p>
+          <input class=" font-extrabold bg-gradient-to-tr from-[#025F60] to-[#025F60] bg-clip-text text-transparent border rounded-md">
+          <p class="text-base text-gray-500">Hasło</p>
+          <input class=" font-extrabold bg-gradient-to-tr from-[#025F60] to-[#025F60] bg-clip-text text-transparent border rounded-md"><br>
+          <input id="remember_password" type="checkbox" name="remember_password">
+          <label for="remember_password">Zapamiętaj mnie</label>
+          <p>zapomniałeś hasła?</p>
+          <br>
+          <button class="bg-black rounded-lg w-1/2 h-8 text-white" type="submit">Zaloguj się</button>
         </form>
+        <div>
+          <p>Zaloguj się za pomocą Facebook</p>
+        </div>
       </div>
-    </template>
-    <template #right>
-      <h2>Welcome Back!</h2>
-      <p>Please login to access your dashboard.</p>
     </template>
   </AuthLayout>
 </template>
-
-<style scoped>
-
-</style>
