f3a9e6975f82850a701b2d8cadc4c18e629b462a
Dominikaninn <130690231+PrabuckiDominik@users.noreply.github.com>
2025-06-25 10:40:02 +0200

    Update database/factories/UserFactory.php
    Co-authored-by: Aleksandra Kozubal <104600942+AleksandraKozubal@users.noreply.github.com>

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ class UserFactory extends Factory
     {
         return [
             "first_name" => fake()->firstName(),
-            "last_name" => fake()->lastName,
+            "last_name" => fake()->lastName(),
             "email" => fake()->unique()->safeEmail(),
             "email_verified_at" => now(),
             "password" => Hash::make("password"),
