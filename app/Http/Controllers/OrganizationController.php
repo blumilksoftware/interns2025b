0e98908070054de09302c830bfd3e28a6d3d4c3c
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2025-06-27 08:55:05 +0200

    Refactor `OrganizationController` to use `OrganizationResource` and simplify routes with resource grouping.

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Interns2025b\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
+use Interns2025b\Http\Resources\OrganizationResource;
 use Interns2025b\Models\Organization;
 
 class OrganizationController extends Controller
@@ -14,16 +15,14 @@ class OrganizationController extends Controller
         $organizations = Organization::query()->with(["owner", "users"])->get();
 
         return response()->json([
-            "data" => $organizations,
+            "data" => OrganizationResource::collection($organizations),
         ]);
     }
 
-    public function show(int $id): JsonResponse
+    public function show(Organization $organization): JsonResponse
     {
-        $organization = Organization::query()->with(["owner", "users"])->findOrFail($id);
-
         return response()->json([
-            "data" => $organization,
+            "data" => new OrganizationResource($organization),
         ]);
     }
 }
