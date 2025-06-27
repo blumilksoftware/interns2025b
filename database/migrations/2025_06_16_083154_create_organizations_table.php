78c05537856afa077a63e6157b0a9b91c0a537b6
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2025-06-27 07:34:37 +0200

    Refactor migrations to use `foreignIdFor` syntax replacing `foreignId`

--- database/migrations/2025_06_16_083154_create_organizations_table.php
+++ database/migrations/2025_06_16_083154_create_organizations_table.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Interns2025b\Models\User;
 
 return new class() extends Migration {
     public function up(): void
@@ -12,7 +13,7 @@ return new class() extends Migration {
         Schema::create("organizations", function (Blueprint $table): void {
             $table->id();
             $table->string("name");
-            $table->foreignId("owner_id")->nullable()->constrained("users")->nullOnDelete();
+            $table->foreignIdFor(User::class, "owner_id")->nullable()->constrained()->nullOnDelete();
             $table->string("group_url")->nullable();
             $table->string("avatar_url")->nullable();
             $table->timestamps();
