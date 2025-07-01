f352e28a37af3bf943e415d736c6b9ff9d26e259
Dominik Prabucki <dominikprabucki.2002@gmail.com>
2025-07-01 10:42:42 +0200

    Declare strict types in `UserResource` and update array keys to use double quotes for consistency.

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Interns2025b\Http\Resources;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
@@ -10,13 +11,13 @@ class UserResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            'id' => $this->id,
-            'first_name' => $this->first_name,
-            'last_name' => $this->last_name,
-            'email' => $this->email,
-            'email_verified_at' => $this->email_verified_at,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            "id" => $this->id,
+            "first_name" => $this->first_name,
+            "last_name" => $this->last_name,
+            "email" => $this->email,
+            "email_verified_at" => $this->email_verified_at,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
         ];
     }
 }
