3a03abb2dd6c58a2408e4fd0dea91fea4380f559
Jakub Demkowski <jakub.demkowski@studenci.collegiumwitelona.pl>
2025-07-14 13:55:43 +0200

    draft for home page

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -1,8 +1,32 @@
-
 <template>
-  <div>
-    <h1>HomePage</h1>
-    <a href="/login">Login</a><br>
-    <a href="/register">register</a>
+  <div class="w-full bg-fuchsia-500 flex flex-col sm:items-center sm:justify-center">
+    <div class="flex w-full bg-blue-300 items-center">
+      <h1>navbar</h1>
+      <div class="">
+        <a href="/login">zaloguj się</a><br>
+        <a href="/register">zarejestruj się</a>
+        <img class="mb-10" src="/images/LogoWhite2.png" alt="LetsEvent">
+      </div>
+    </div>
+    <div class="sm:w-5/6 flex flex-col bg-gray-500">
+      <div class="border-gray-600 border-2 space-y-10">
+        <div class="w-full justify-center bg-green-500">
+          <h1 class="text-6xl">Znajdz Wydarzenie w twojej okolicy</h1>
+        </div>
+        <div class="w-full h-96 bg-red-500">
+          <h1>mapa</h1>
+        </div>
+      </div>
+    </div>
+    <div class="w-full bg-gradient-to-tr from-brand to-brand-light ">
+      <h1>bądz na bieżąnco dołącz do LetsEvent</h1>
+      <p>Dodaj własne wydarzenie</p>
+      <p>Bierz udział w wydarzeniach</p>
+      <a href="/register">zarejestruj się</a>
+      Wszystkie prawa zastrzeżone © 2025 Interns2025b
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+</script>
