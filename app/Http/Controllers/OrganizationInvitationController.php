8efc68b057b4e6c1697862be66a9470c1513752e
Adam Kasprzyk <ada.kasprzyk@studenci.collegiumwitelona.pl>
2025-07-17 09:20:57 +0200

    Fixes accordnig to code review.

--- app/Http/Controllers/OrganizationInvitationController.php
+++ app/Http/Controllers/OrganizationInvitationController.php
@@ -7,7 +7,6 @@ namespace Interns2025b\Http\Controllers;
 use Illuminate\Contracts\Mail\Mailer;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\URL;
 use Interns2025b\Http\Requests\SendOrganizationInvitationRequest;
 use Interns2025b\Mail\OrganizationInvitationMail;
 use Interns2025b\Models\Organization;
@@ -19,16 +18,7 @@ class OrganizationInvitationController extends Controller
     {
         $this->authorize("invite", $organization);
 
-        $url = URL::temporarySignedRoute(
-            "organizations.accept-invite",
-            now()->addHours(48),
-            [
-                "organization" => $organization->id,
-                "email" => $request->email,
-            ],
-        );
-
-        $mailer->to($request->email)->send(new OrganizationInvitationMail($organization->name, $url));
+        $mailer->to($request->email)->send(new OrganizationInvitationMail($organization));
 
         return response()->json([
             "message" => __("organization.invitation_sent"),
--- app/Mail/OrganizationInvitationMail.php
+++ app/Mail/OrganizationInvitationMail.php
@@ -7,6 +7,8 @@ namespace Interns2025b\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\URL;
+use Interns2025b\Models\Organization;
 
 class OrganizationInvitationMail extends Mailable
 {
@@ -14,13 +16,24 @@ class OrganizationInvitationMail extends Mailable
     use SerializesModels;
 
     public function __construct(
-        public string $organizationName,
-        public string $url,
+        public Organization $organization
     ) {}
 
-    public function build()
+    public function build(): self
     {
-        return $this->subject(__("organization.invitation_subject"))
-            ->view("emails.organization-invitation");
+        $url = URL::temporarySignedRoute(
+            "organizations.accept-invite",
+            now()->addHours(48),
+            [
+                "organization" => $this->organization->id,
+                "email" => $this->to[0]["address"],
+            ],
+        );
+
+        return $this->view("emails.organization-invitation")
+            ->with([
+                "organizationName" => $this->organization->name,
+                "url" => $url,
+            ]);
     }
 }
