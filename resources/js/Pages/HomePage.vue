966499c0ed83f35b250b6b1a4b2e2e0b20aded37
Jakub Demkowski <jakub.demkowski@studenci.collegiumwitelona.pl>
2025-07-17 13:40:01 +0200

    Add `leaflet` and `vue-i18n` libraries, integrate a map component using Leaflet in `HomePage.vue` and `EventPage.vue`, refactor layout and interactivity for enhanced event details, and improve navbar design and usability.

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
 import { Link as InertiaLink } from '@inertiajs/vue3'
-import navbar from '@/Components/navbar.vue'
+import Navbar from '@/Components/Navbar.vue'
+import Map from '@/Components/Map.vue'
 </script>
 
 <template>
   <app-head title="Home Page" />
   <div class="w-full flex flex-col sm:items-center sm:justify-center">
-    <div class="flex w-full">
+    <div class="flex w-full mb-32">
       <navbar>
         <h1 class="font-bold text-6xl">
           Znajdź
-          <InertiaLink href="#" class="text-gradient-teal">
+          <span class="text-gradient-teal">
             wydarzenie
-          </InertiaLink><br>
+          </span><br>
           w Twojej okolicy
         </h1>
       </navbar>
@@ -20,39 +21,45 @@ import navbar from '@/Components/navbar.vue'
     <div class="sm:w-5/6 flex flex-col">
       <div class="mb-36">
         <div class="w-full h-[777px] bg-[#E5E7EB] rounded-3xl">
-          <h1>mapa</h1>
+          <Map />
         </div>
       </div>
     </div>
     <div
-      class="w-full bg-gradient-to-tr from-brand font-normal to-brand-light pb-8"
+      class="w-full bg-gradient-to-tr from-brand font-normal to-brand-light py-16"
     >
       <div
         class="flex flex-col items-center justify-center text-center text-white space-y-16"
       >
         <h1 class="text-6xl font-bold pt-6">
-          Bądz na bieżąnco<br>
+          Bądź na bieżaco<br>
           <span class="text-gradient-teal-light">dołącz do LetsEvent</span>
         </h1>
         <div class="flex font-normal justify-center gap-8">
-          <p>Dodaj własne wydarzenie</p>
-          <p>Bierz udział w wydarzeniach</p>
+          <inertia-link href="/event" class="border border-[#FFFFFF1A] rounded-full px-[13px] py-[5px] ">Dodaj własne wydarzenie</inertia-link>
+          <inertia-link href="/event" class="border border-[#FFFFFF1A] rounded-full px-[13px] py-[5px] ">Bierz udział w wydarzeniach</inertia-link>
         </div>
-        <InertiaLink href="/register" class="text-black">
+        <InertiaLink href="/register" class="bg-white font-normal rounded-full text-black shadow-shadow-blue hover:scale-105 shadow-sm p-3">
           zarejestruj się
         </InertiaLink>
         <div class="w-4/6 flex justify-between text-gray-500">
           <p>Wszystkie prawa zastrzeżone © 2025 Interns2025b</p>
-          <div>
+          <div class="flex gap-8 items-center">
             <ul class="flex items-center gap-4">
               <li>
-                <a href="/regulamin" class="hover:underline">Regulamin</a>
+                <InertiaLink href="/regulamin" class="hover:underline hover:text-gray-400">Regulamin</InertiaLink>
               </li>
               <li aria-hidden="true">•</li>
               <li>
-                <a href="/polityka-prywatnosci" class="hover:underline">Polityka prywatności</a>
+                <InertiaLink href="/polityka-prywatnosci" class="hover:underline hover:text-gray-400">Polityka prywatności</InertiaLink>
               </li>
             </ul>
+            <div class="flex items-center gap-4">
+              <a href="#">X</a>
+              <a href="#">Ln</a>
+              <a href="#">YT</a>
+              <a href="#">Slack</a>
+            </div>
           </div>
         </div>
       </div>
