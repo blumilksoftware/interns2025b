a2c482b1bfea0d87ad25d3236be162defb483e74
Adam Kasprzyk <adam.kasprzyk@studenci.collegiumwitelona.pl>
2025-07-29 13:00:38 +0200

    Merge fix

--- app/Http/Controllers/UpdatePasswordController.php
+++ app/Http/Controllers/UpdatePasswordController.php
@@ -4,28 +4,21 @@ declare(strict_types=1);
 
 namespace Interns2025b\Http\Controllers;
 
-use Illuminate\Cache\RateLimiter;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Interns2025b\Actions\ThrottleAction;
 use Interns2025b\Http\Requests\UpdatePasswordRequest;
 use Symfony\Component\HttpFoundation\Response as Status;
 
 class UpdatePasswordController extends Controller
 {
-    public function updatePassword(UpdatePasswordRequest $request): JsonResponse
+    public function updatePassword(UpdatePasswordRequest $request, ThrottleAction $throttle): JsonResponse
     {
         $user = Auth::user();
-        $limiter = app(RateLimiter::class);
         $key = "password-update:" . $user->id;
 
-        if ($limiter->tooManyAttempts($key, 1)) {
-            return response()->json([
-                "message" => __("passwords.throttled"),
-            ], Status::HTTP_TOO_MANY_REQUESTS);
-        }
-
-        $limiter->hit($key, 900);
+        $throttle->handle($key, "15min", "passwords.throttled");
 
         $newPassword = $request->getNewPassword();
 
